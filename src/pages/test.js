e79ee1b74e08af2344267e4da41225014c1b0cf5
squteronsquter <squter@squter.pl>
2021-09-19 11:03:14 +0200

    test.js page edited to remove warnings

--- src/pages/test.js
+++ src/pages/test.js
@@ -1,8 +1,6 @@
 import React from "react"
 import Layout from "../components/Layout"
 import "../assets/css/app.scss"
-import { StaticImage } from "gatsby-plugin-image"
-import { Link } from "gatsby"
 
 export default function Test() {
   return (
